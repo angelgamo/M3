0811881ba46193c6a397d38d640f0dc86061f085
angelgamo <garcia.angel.69420@gmail.com>
2021-11-25 16:35:13 +0100

    optimize uf5 p03 stackstatemachine

--- UF5/m03uf5p03-GarciaAngel/src/StackStateMachine.java
+++ UF5/m03uf5p03-GarciaAngel/src/StackStateMachine.java
@@ -1,65 +1,30 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
-public class StackStateMachine extends StateMachine {
-	class Stack<T> {
-		ArrayList<T> list;
-		
-		public Stack() {
-			super();
-			this.list = new ArrayList<T>();
-		}
-		
-		public void add(T item) {
-			list.add(item);
-		}
-		
-		public void set(T item) {
-			list.set(list.size()-1, item);
-		}
-		
-		public T get() {
-			if (!list.isEmpty()) {
-				return list.get(list.size()-1);
-			}
-			return null;
-		}
-		
-		public void pop() {
-			if (!list.isEmpty()) {
-				list.remove(list.get(list.size()-1));
-			}
-		}
-		
-		public int size() {
-			return list.size();
-		}
-	}
-	
+public class StackStateMachine {
+	Set<State> states;
 	Stack<State> stackStates;
 	
 	public StackStateMachine() {
 		super();
+		this.states = new HashSet<State>();
 		this.stackStates = new Stack<State>();
 	}
 	
-	@Override
 	public void addState(State state) {
-		if (currentState == null) {
+		if (stackStates.isEmpty()) {
 			stackStates.add(state);
-			currentState = stackStates.get();
-			currentState.init();
+			stackStates.lastElement().init();
 		}
 		states.add(state);
 	}
 	
-	@Override
 	public void setState(Class<? extends State> stateClass) {
 		for (State state : states) {
 			if (state.getClass() == stateClass) {
-				currentState.exit();
-				stackStates.set(state);
-				currentState = stackStates.get();
-				currentState.init();
+				stackStates.pop().exit();
+				stackStates.push(state).init();
 			}
 		}
 	}
@@ -67,20 +32,20 @@ public class StackStateMachine extends StateMachine {
 	public void pushState(Class<? extends State> stateClass) {
 		for (State state : states) {
 			if (state.getClass() == stateClass) {
-				currentState.exit(); // Maybe
-				stackStates.add(currentState);
-				currentState = stackStates.get();
-				currentState.init();
+				stackStates.lastElement().exit();
+				stackStates.push(state).init();
 			}
 		}
 	}
 	
 	public void popState() {
 		if (stackStates.size() > 1) {
-			currentState.exit();
-			stackStates.pop();
-			currentState = stackStates.get();
-			currentState.init();
+			stackStates.pop().exit();
+			stackStates.lastElement().init();
 		}
+	}
+	
+	public void update() {
+		stackStates.get(stackStates.size()-1).update();
 	}
 }
